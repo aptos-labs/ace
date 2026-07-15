ca9e8200f8c0eba2b3e4ccab781a2c04a8e2460e
zhoujunma <zhoujun@aptoslabs.com>
2026-07-15 01:01:28 -0700

    Fix Shelby private beta chain ID (#176)
    * Fix Shelby private beta chain id
    
    * Remove unnecessary Shelby chain ID test

--- worker-components/network-node/src/config.rs
+++ worker-components/network-node/src/config.rs
@@ -14,7 +14,7 @@ pub struct ChainRpcConfig {
     pub aptos_mainnet: AptosRpc,                     // chain_id=1
     pub aptos_testnet: AptosRpc,                     // chain_id=2
     pub aptos_localnet: AptosRpc,                    // chain_id=4
-    pub aptos_shelby_private_beta: Option<AptosRpc>, // chain_id=139
+    pub aptos_shelby_private_beta: Option<AptosRpc>, // chain_id=125
 }
 
 impl ChainRpcConfig {
@@ -23,9 +23,9 @@ impl ChainRpcConfig {
             1 => Ok(&self.aptos_mainnet),
             2 => Ok(&self.aptos_testnet),
             4 => Ok(&self.aptos_localnet),
-            139 => self.aptos_shelby_private_beta.as_ref().ok_or_else(|| {
+            125 => self.aptos_shelby_private_beta.as_ref().ok_or_else(|| {
                 anyhow!(
-                    "no Aptos RPC configured for chain_id 139 (shelby-private-beta); \
+                    "no Aptos RPC configured for chain_id 125 (shelby-private-beta); \
                      set --aptos-shelby-private-beta-api"
                 )
             }),

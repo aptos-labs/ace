4e88c26bbb1e2c7a6b77a8928e2b7527014758d4
zhoujunma <zhoujun@aptoslabs.com>
2026-07-16 13:50:37 -0700

    Add Shelby beta USCE1 known deployment (#182)

--- ts-sdk/src/known-deployments.ts
+++ ts-sdk/src/known-deployments.ts
@@ -28,4 +28,13 @@ export const knownDeployments = {
         ibeKeypairId: AccountAddress.fromString('0xbb83c1eb79580d9e23639fa28373047f64d2c8bd3526590d2d886cf91fb5a307'),
         vrfKeypairId: AccountAddress.fromString('0x3ca79722e34031f87ef5be65890d2c12d742390a641d9b1f0333155eda67dd9d'),
     }),
+    'shelby-beta-usce1': knownDeployment({
+        chainId: 125,
+        aceDeployment: new AceDeployment({
+            apiEndpoint: 'https://api.shelby-beta.teraswitch.aptosdev.com/v1',
+            contractAddr: AccountAddress.fromString('0x086f9a291d3d28140413505f6224d10e07cb6d6d08ab5933f62ff1b685830408'),
+        }),
+        ibeKeypairId: AccountAddress.fromString('0x50ca2eb86412416256522777770b9846ced2b0185db1d301f233d5f47215f4c3'),
+        vrfKeypairId: AccountAddress.fromString('0xf47b51b8c648a3dd53a1c0ec5d38e2b861f0b6d4c3181f0b84b0d535e274a98d'),
+    }),
 } as const;
